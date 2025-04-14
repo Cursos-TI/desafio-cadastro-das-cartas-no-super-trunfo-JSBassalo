5447672ecce2264d1ea7d2344f33a0529e48d833
JSBassalo <jonathansbassalo@gmail.com>
2025-04-14 08:05:09 +0000

    Nível Novato: Cadastro Básico

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -19,7 +19,7 @@ int main() {
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
     //Variaveis
-    char estado1[100], estado2[100];
+    char estado1, estado2;
     char codigo_da_carta1[4], codigo_da_carta2[4];
     char nome_da_cidade1[100], nome_da_cidade2[100];
     int populacao1, populacao2;
